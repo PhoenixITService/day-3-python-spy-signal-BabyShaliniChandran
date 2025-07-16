e4b67067dc1f2d26ddd7369f76d06e31a7102803
BabyShaliniChandran <Baby.Chandran@Coforge.com>
2025-07-16 17:51:14 +0530

    Update shalini_spy.py

--- shalini_spy.py
+++ shalini_spy.py
@@ -1,7 +1,7 @@
 input=list(map(int,input("Enter the input:").split()))
 sequence=input[1::3]
 reversed=sequence[::-1]
-output=""
+output=[]
 for num in reversed:
     output+=chr(num)
-print(output)
+print("".join(output))
